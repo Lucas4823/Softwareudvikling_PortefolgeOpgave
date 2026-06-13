87fd6d072298db5ef1455c10734be0a31cd121ca
Lucas Nguyen <lucasnguyen4823@gmail.com>
2026-06-14 01:36:48 +0200

    Tilføjet Kamp klasse

---
+++ include/Kamp.h
@@ -0,0 +1,9 @@
+#pragma once
+#include "Monster.h"
+
+class Kamp
+{
+public:
+    bool kaemp(Monster& spillerMonster,
+               Monster& fjendeMonster);
+};
---
+++ src/Kamp.cpp
@@ -0,0 +1,37 @@
+#include "Kamp.h"
+#include <cstdlib>
+
+bool Kamp::kaemp(Monster& spillerMonster,
+                 Monster& fjendeMonster)
+{
+    bool spillerStarter = rand() % 2;
+
+    while (!spillerMonster.erBesejret()
+           && !fjendeMonster.erBesejret())
+    {
+        if (spillerStarter)
+        {
+            fjendeMonster.tagSkade(
+                spillerMonster.getStyrke());
+
+            if (!fjendeMonster.erBesejret())
+            {
+                spillerMonster.tagSkade(
+                    fjendeMonster.getStyrke());
+            }
+        }
+        else
+        {
+            spillerMonster.tagSkade(
+                fjendeMonster.getStyrke());
+
+            if (!spillerMonster.erBesejret())
+            {
+                fjendeMonster.tagSkade(
+                    spillerMonster.getStyrke());
+            }
+        }
+    }
+
+    return !spillerMonster.erBesejret();
+}
